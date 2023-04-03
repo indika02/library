796987af841a16cbceed054797d2e56610f9a360
indika02 <indikai002@gmail.com>
2023-04-03 15:32:44 +0530

    Login page start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import Login from './pages/login/Login';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
